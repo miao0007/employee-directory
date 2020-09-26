e554c815c19f92b69877e922b8ed0929ed39e2b2
miao0007 <miao188@hotmail.com>
2020-09-26 20:03:23 +0930

    add Footer.js

---
+++ employee_directory/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="m-10 text-sm text-center text-gray-500 bg-light">
+ <p>Built by <a href="https://github.com/miao0007" className="font-bold text-gray-600 hover:text-yellow-500">Jeff Miao</a>  @ 2020 </p>
+        </div>
+    )
+}
+
+export default Footer;
