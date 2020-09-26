eb932925198b51290cd48db51be21e293d01adee
miao0007 <miao188@hotmail.com>
2020-09-26 20:21:25 +0930

    modified index.js

--- employee_directory/src/index.js
+++ employee_directory/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App />,
+
   document.getElementById('root')
 );
 
