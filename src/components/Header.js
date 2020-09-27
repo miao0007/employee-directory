f38cb19351feab55cd3f88ec03727d3dd0d83fd2
miao0007 <miao188@hotmail.com>
2020-09-27 20:28:00 +0930

    changed header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-    <div className = "p-3 mb-2 bg-light text-dark">
+    <div className = "shadow-sm p-3 mb-2 bg-light text-dark rounded">
 <header className="container max-w-6xl px-10 py-3 mx-auto flex justify-between items-center">
     <h1 className="title text-5xl text-gray-800 mt-16">Employee Directory</h1>
 <p className="mb-16 text-md">Search for an employee or sort by Name/Category.</p>
